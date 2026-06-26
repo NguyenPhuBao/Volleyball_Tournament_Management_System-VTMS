6985d56012b955ee43c8862a631145ef49b0f007
NguyenPhuBao <153645756+NguyenPhuBao@users.noreply.github.com>
2026-06-27 00:08:41 +0700

    Delete laravel directory

--- laravel/resources/views/dashboard/index.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.main')
-
-@section('content')
-    <section>
-        <h1>{{ $moduleTitle ?? 'Dashboard' }}</h1>
-        <p>Xin chao {{ $user['name'] ?? $user['username'] ?? 'VTMS' }}.</p>
-    </section>
-@endsection
--- laravel/resources/views/errors/404.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.main')
-
-@section('content')
-    <h1>404</h1>
-    <p>Khong tim thay trang.</p>
-@endsection
